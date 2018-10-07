f90560267bcb73d7d82b103f7d13809cc1bc7249
virtik1 <lukish.vlad.2000@gmail.com>
2018-10-07 21:13:10 +0300

    changed binarySearch.java

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,10 +1,6 @@
 public class BinarySearch {
 
     public static void main(String[] args) {
-//        int[] array = {0, -1, 20, 15, -3, 60, 70};
-//        SelectionSort.selectionSort(array);
-//        SelectionSort.print(array);
-//        System.out.println(binarySearch(new int[] {-1, 10, 30, 31, 45, 60}, 61));
     }
 
     public static int binarySearch(int[] list, int number) {
